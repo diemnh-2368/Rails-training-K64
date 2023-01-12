f93536dc2d4000507a9c5e773f36560b50eef4aa
diemnh <nguyen.hong.diem@sun-asterisk.com>
2023-01-12 15:44:05 +0000

    Add Home

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+    
+  get 'static_pages/home'
+  root 'static_pages#home'
+  
   devise_for :users
   resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
