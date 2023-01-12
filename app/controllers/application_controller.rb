38440346bbd96bcc07b29ab998e9a61f33c8dcec
diemnh <nguyen.hong.diem@sun-asterisk.com>
2023-01-12 16:47:29 +0000

    Add avatar on users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,8 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   protected
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit :sign_up, keys: [:name, :age]
-      devise_parameter_sanitizer.permit :account_update, keys: [:name, :age]
+      devise_parameter_sanitizer.permit :sign_up, keys: [:name, :age, :avatar]
+      devise_parameter_sanitizer.permit :account_update, keys: [:name, :age, :avatar]
     end
 
 end
