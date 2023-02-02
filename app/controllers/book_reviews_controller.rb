a1223e4aee8a38988546edc41dfc1120c4981ddd
hwngngueyn <hunggithub.work@gmail.com>
2023-02-02 09:15:49 +0000

    add review list screen

--- app/controllers/book_reviews_controller.rb
+++ app/controllers/book_reviews_controller.rb
@@ -26,6 +26,7 @@ class BookReviewsController < ApplicationController
 
     respond_to do |format|
       if @book_review.save
+        url = "/books/" + @book_review.book_id.to_s
         format.html { redirect_to book_review_url(@book_review), notice: "Book review was successfully created." }
         format.json { render :show, status: :created, location: @book_review }
       else
